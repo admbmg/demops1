2b05722a52d4016299b8d5d0387a34909283ee1e
admbmg <74906735+admbmg@users.noreply.github.com>
2022-10-17 19:36:45 +0200

    Update rb-apim-backup-dev.ps1

--- rb-apim-backup-dev.ps1
+++ rb-apim-backup-dev.ps1
@@ -1,18 +1,18 @@
 Param
 (
     [Parameter (Mandatory= $false)]
-    [string]$umiAppId='2c1101c6-a761-4483-bb60-de1564739c54'
+    [string]$umiAppId='2864b0f8-fd9b-4056-b045-542d9c2dbf2d'
 )
 
 
-# Ensures you do not inherit an AzContext in your runbook
+# Ensures AzContext is not inherited.
 Disable-AzContextAutosave -Scope Process
 
 # Get UMI App ID - Does not work without context
 # (Get-AzADServicePrincipal -DisplayName uamiautacc1).AppId
 
 # Connect to Azure with user-assigned managed identity
-$AzureContext = (Connect-AzAccount -Identity -AccountId 2c1101c6-a761-4483-bb60-de1564739c54).context
+$AzureContext = (Connect-AzAccount -Identity -AccountId $umiAppId).context
 
 # set and store context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
@@ -24,12 +24,12 @@ $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -Defa
 
 $apiManagementName="bmgapimtest1";
 $apiManagementResourceGroup="DemoRG001";
-$storageAccountName="demoback2";
-$storageResourceGroup="DemoRG001";
-$containerName="backup1";
+$storageAccountName="audevinautbackup";
+$storageResourceGroup="AU-Common-Backup-Resources";
+$containerName="forbackup01";
 $blobName="apimbackup_" + (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss") 
 
-$identityName = "uamitest";
+$identityName = "audev-integration-apimgr-mi";
 $identityResourceGroup = "DemoRG001";
 
 $identityId = (Get-AzUserAssignedIdentity -Name $identityName -ResourceGroupName $identityResourceGroup).ClientId
